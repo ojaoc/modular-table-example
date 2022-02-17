395d60333707303ced81584d893ac9a4666e1c64
Joao Correia (CTW) <Joao.FC.Correia@ctw.bmwgroup.com>
2022-02-17 13:01:12 +0000

    Example table using list and divs with reordering of columns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { DragDropModule } from "@angular/cdk/drag-drop";
-import { registerLocaleData } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
-import en from "@angular/common/locales/en";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { en_US, NZ_I18N } from "ng-zorro-antd/i18n";
-import { NzIconModule } from "ng-zorro-antd/icon";
-import { NzTableModule } from "ng-zorro-antd/table";
-import { AppComponent } from "./app.component";
-import { TableComponent } from "./shared/components/table/table.component";
+import { DragDropModule } from '@angular/cdk/drag-drop'
+import { registerLocaleData } from '@angular/common'
+import { HttpClientModule } from '@angular/common/http'
+import en from '@angular/common/locales/en'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NzGridModule } from 'ng-zorro-antd/grid'
+import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n'
+import { NzIconModule } from 'ng-zorro-antd/icon'
+import { NzListModule } from 'ng-zorro-antd/list'
+import { AppComponent } from './app.component'
+import { TableComponent } from './shared/components/table/table.component'
 
-registerLocaleData(en);
+registerLocaleData(en)
 
 @NgModule({
   declarations: [AppComponent, TableComponent],
@@ -21,9 +22,10 @@ registerLocaleData(en);
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    NzTableModule,
+    NzListModule,
     DragDropModule,
     NzIconModule,
+    NzGridModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
