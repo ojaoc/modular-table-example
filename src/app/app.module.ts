7703f044aeb3d5509bebd46a9090b3e9f74c301f
João Correia <correia.jafonso@gmail.com>
2022-02-17 15:26:06 +0000

    Div table (#2)
    * Example table using list and divs with reordering of columns
    
    * Added min-width to columns
    
    * Implemented pagination in table
    
    Co-authored-by: Joao Correia (CTW) <Joao.FC.Correia@ctw.bmwgroup.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,34 @@
-import { DragDropModule } from "@angular/cdk/drag-drop";
-import { registerLocaleData } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
-import en from "@angular/common/locales/en";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { en_US, NZ_I18N } from "ng-zorro-antd/i18n";
-import { NzIconModule } from "ng-zorro-antd/icon";
-import { NzTableModule } from "ng-zorro-antd/table";
-import { AppComponent } from "./app.component";
-import { TableComponent } from "./shared/components/table/table.component";
+import { DragDropModule } from '@angular/cdk/drag-drop'
+import { registerLocaleData } from '@angular/common'
+import { HttpClientModule } from '@angular/common/http'
+import en from '@angular/common/locales/en'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NzGridModule } from 'ng-zorro-antd/grid'
+import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n'
+import { NzIconModule } from 'ng-zorro-antd/icon'
+import { NzListModule } from 'ng-zorro-antd/list'
+import { NzPaginationModule } from 'ng-zorro-antd/pagination'
+import { AppComponent } from './app.component'
+import { TableComponent } from './shared/components/table/table.component'
+import { PaginationPipe } from './shared/pipes/pagination.pipe'
 
-registerLocaleData(en);
+registerLocaleData(en)
 
 @NgModule({
-  declarations: [AppComponent, TableComponent],
+  declarations: [AppComponent, TableComponent, PaginationPipe],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    NzTableModule,
+    NzListModule,
     DragDropModule,
     NzIconModule,
+    NzGridModule,
+    NzPaginationModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,27 +1,39 @@
-import { CdkDragSortEvent, moveItemInArray } from "@angular/cdk/drag-drop";
-import { Component, OnInit } from "@angular/core";
-import { dataset } from "src/app/core/mocks/dataset";
-import { Data } from "src/app/core/models/data";
+import { CdkDragSortEvent, moveItemInArray } from '@angular/cdk/drag-drop'
+import { Component, OnInit } from '@angular/core'
+import { dataset } from 'src/app/core/mocks/dataset'
+import { Data } from 'src/app/core/models/data'
 
 @Component({
-  selector: "app-table",
-  templateUrl: "./table.component.html",
-  styleUrls: ["./table.component.scss"],
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit {
-  dataSet: Data;
+  dataSet: Data
+  page: number
+  size: number
 
   constructor() {
-    this.dataSet = dataset;
+    this.dataSet = dataset
+    this.page = 1
+    this.size = 10
   }
 
   ngOnInit(): void {}
 
-  sort(event: CdkDragSortEvent<{ name: string; title: string }[]>) {
+  reorderCol(event: CdkDragSortEvent<{ name: string; title: string }[]>) {
     moveItemInArray(
       event.container.data,
       event.previousIndex,
       event.currentIndex
-    );
+    )
+  }
+
+  changePage(newPage: number) {
+    this.page = newPage
+  }
+
+  changeSize(newSize: number) {
+    this.size = newSize
   }
 }
